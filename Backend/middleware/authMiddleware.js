8fad9bf0666b3e91de9d1435cc1a5a265e4807aa
iam-souvik <souvikpatramax@gmail.com>
2023-03-06 18:57:01 +0530

    chat_application

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -8,9 +8,10 @@ const protect = asyncHandler(async (req, res, next) => {
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
-    
+
   ) {
     try {
+      
       token = req.headers.authorization.split(" ")[1];
 
 
