477839267f83a43a3c250b7e60255bc6499c71df
iam-souvik <souvikpatramax@gmail.com>
2023-03-06 18:56:47 +0530

    chat_application

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -8,10 +8,11 @@ const protect = asyncHandler(async (req, res, next) => {
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
+    
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
-      
+
 
       //decodes token id
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
